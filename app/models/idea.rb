d7e2edb202bc7f601637da648563162550216d9f
Diana Thiele <diana.mail@thiele.me>
2016-04-28 21:55:13 +0200

    Relationship User-Idea

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,4 +1,5 @@
 class Idea < ActiveRecord::Base
   has_many :comments
+  belongs_to :user
   mount_uploader :picture, PictureUploader
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160427200138) do
+ActiveRecord::Schema.define(version: 20160428192408) do
 
   create_table "comments", force: :cascade do |t|
     t.string   "user_name"
@@ -27,7 +27,10 @@ ActiveRecord::Schema.define(version: 20160427200138) do
     t.string   "picture"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "user_id"
   end
+
+  add_index "ideas", ["user_id"], name: "index_ideas_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
